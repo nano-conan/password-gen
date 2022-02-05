54a8f87b7e19c6f40cbe06e106f1c6a7aa1cd329
Conan <csimon@unb.ca>
2022-02-05 17:49:02 -0400

    Creates a password with random unicode characters

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Password Generator!");
+    print!("How many characters does your password need to be? ");
+
+    let mut input = String::new();
+    let _ = io::stdout().flush();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("error: unable to read user input");
+
+    let char_length: i32 = input.trim().parse::<i32>().unwrap();
+
+    let mut count = 0;
+
+    let mut pw = String::new();
+
+    loop{
+        count += 1;
+
+        pw = format!("{}{}",pw, rand::random::<char>());
+
+        if count >= char_length
+        {
+            break;
+        }
+    }
+
+    println!("Your pw is : {}", pw);
 }
+
